0d63aa3ab343cc570285f47cb382d5e9a742605d
JoseRogerioDaSilva <202502051271@alunos.estacio.br>
2025-03-04 13:47:53 +0000

    Codigo finalizado mas farei otimizacoes

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,7 +2,7 @@
 
 int main() {
 printf("Bem vindo ao Desafio das cartas de super trunfo");
-print("\n");
+printf("\n");
 
 char codigo1[5], codigo2[5], estado1[10], estado2[10], cidade1[30], cidade2[30];
 unsigned long int populacao1, populacao2;
@@ -20,7 +20,7 @@ printf("digite o nome da cidade\n");
 scanf("%s", cidade1);
 
 Printf("digite a populacao da carta\n");
-scanf("%s", cidade1);
+scanf("%li", populacao1);
 
 printf("digite a area em km\n");
 scanf("%f", &area1);
@@ -43,14 +43,14 @@ scanf("%s", estado2);
 printf("digite o nome da cidade\n");
 scanf("%s", cidade2);
 
-printf("digite a populacao da carta\n")
-scanf("%d", &populacao2);
+printf("digite a populacao da carta\n");
+scanf("%li", &populacao2);
 
 printf("digite a area em km\n");
-scanf("&f", area2);
+scanf("%f", &area2);
 
 printf("digite o PIB\n");
-scanf("%f", PIB2);
+scanf("%f", &PIB2);
 
 printf("digite a quantidade de pontos turisticos\n");
 scanf("%d", &pontosturisticos2);
@@ -62,7 +62,55 @@ PIBCap1 = (float)PIB1 / populacao1;
 PIBCap2 = (float)PIB2 / populacao2;
 Superpoder1 = (float)1/ populacao1 + area1 + PIB1 + PIBCap1 + populacao1;
 Superpoder2 = (float)1/ populacao2 + area2 + PIB2 + PIBCap2 + populacao2;
+printf("\n");
 
-
+if(populacao1 > populacao2){
+    printf("Populacao: Carta 1 ganhou");
+}
+else{
+    printf("Populacao: Carta 2 ganhou");
+}
+printf("\n");
+if(area1 > area2){
+    printf("Area: Carta 1 ganhou");
+}
+else{
+    printf("Area: Carta 2 ganhou");
+}
+printf("\n");
+if(PIB1 > PIB2){
+    printf("PIB: Carta 1 ganhou");
+}
+else{
+    printf("PIB: Carta 2 ganhou");
+}
+printf("\n");
+if(pontosturisticos1 > pontosturisticos2){
+    printf("Pontos turisticos: Carta 1 ganhou");
+}
+else{
+    printf("Pontos turisticos: Carta 2 ganhou");    
+}
+printf("\n");
+if(Densidade1 < Densidade2){
+    printf("Densidade: Carta 1 ganhou");
+}
+else{
+    printf("Densidade: Carta 2 ganhou");
+}
+printf("\n");
+if(PIBCap1 > PIBCap2){
+    printf("PIBperCapta: Carta 1 ganhou");
+}
+else{
+    printf("PIBperCapta: Carta 2 ganhou");
+}
+printf("\n");
+if(Superpoder1 > Superpoder2){
+    printf("Superpoder: Carta 1 ganhou");
+}
+else{
+    printf("Superpoder: Carta 2 ganhou");
+}    
     return 0;
 }
